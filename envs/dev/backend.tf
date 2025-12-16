d268ac27523c7dba2d4a903ce6d1302b76ba66db
bilalyasin889 <56312400+bilalyasin889@users.noreply.github.com>
2025-12-16 15:16:31 +0000

    Align spacing in `backend.tf` for consistency

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "apex-fitness-tf-state-dev"
-    key     = "terraform.tfstate"
-    region  = "eu-west-2"
-    encrypt = true
+    bucket       = "apex-fitness-tf-state-dev"
+    key          = "terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
     use_lockfile = true
   }
 }
