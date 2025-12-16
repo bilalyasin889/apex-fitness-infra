48b3493ae5c8b2d725a83ac0b0167b79615ac54a
bilalyasin889 <56312400+bilalyasin889@users.noreply.github.com>
2025-12-16 15:12:45 +0000

    Add GitHub Actions workflow and terraform configuration for `dev` environment
    - Introduced `.github/workflows/main.yml` to automate Terraform deployment and validation processes.
    - Configured `envs/dev` directory with Terraform files: `main.tf`, `provider.tf`, and `backend.tf`.
    - Added `.gitignore` to exclude Terraform state and IDE-specific files.

---
+++ envs/dev/main.tf
@@ -0,0 +1,49 @@
+locals {
+  default_tags = {
+    CreatedBy   = "Terraform"
+    Environment = "dev"
+    Project     = "apex-fitness"
+  }
+}
+
+# Hosted zone used for app domain
+data "aws_route53_zone" "main" {
+  name = "bilalyasin.com."
+}
+
+# -----------------------------
+# Frontend App
+# -----------------------------
+locals {
+  frontend = {
+    cloudfront_name    = "apex-fitness-dev"
+    origin_bucket_name = "apex-fitness-build-dev"
+
+    domain = "apex-fitness-dev.bilalyasin.com"
+    san    = ["www.apex-fitness-dev.bilalyasin.com"]
+
+    price_class = "PriceClass_100"
+
+    restrictions = {
+      type      = "whitelist"
+      locations = ["GB"]
+    }
+  }
+}
+
+module "app_cloudfront" {
+  source = "git::https://github.com/bilalyasin889/cloudfront_s3.git?ref=v1.0.0"
+
+  name        = local.frontend.cloudfront_name
+  bucket_name = local.frontend.origin_bucket_name
+
+  domain_name               = local.frontend.domain
+  subject_alternative_names = local.frontend.san
+
+  hosted_zone_id = data.aws_route53_zone.main.zone_id
+
+  price_class  = local.frontend.price_class
+  restrictions = local.frontend.restrictions
+
+  tags = local.default_tags
+}
